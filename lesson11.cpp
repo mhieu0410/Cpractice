7ec7dfed4a0afdb7175b7f056f6e037f11fcd396
hieu <mhieu0410@gmail.com>
2024-01-07 21:22:46 +0700

    update

--- lesson11.cpp
+++ lesson11.cpp
@@ -4,8 +4,16 @@ using namespace std;
 
 int main()
 {
-    cout<<"Hello World";
-    cout<<"hi";
-
+    float x,y,s;
+    cout<<" the edge 1:"<<endl;
+    cin>>x;
+    cout<<" the edge 2:"<<endl;
+    cin>>y;
+    s=(x*y)/2;
+    cout<<" the result:"<<s<<endl;
+   
     return 0;
 }
+
+
+//Write a function that takes the base and height of a triangle and return its area.
