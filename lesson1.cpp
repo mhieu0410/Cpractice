066e1ddff4b01e25e9fdc19bd338c60a1909ea40
hieu <mhieu0410@gmail.com>
2024-01-09 14:54:12 +0700

    sumTwoNumbers

--- lesson1.cpp
+++ lesson1.cpp
@@ -2,22 +2,21 @@
 
 using namespace std;
 
-double calculatorTriangleArea( double height, double base){
-return (height*base)/2;
+double sumTwoNumbers( double number1, double number2){
+return (number1 + number2);
 }
-int main()
-{
-    double height, base, area;
-    cout<<" enter height of triangle:"<<endl;
-    cin>>height;
-    cout<<" enter base of triangle:"<<endl;
-    cin>>base;
-    
-    area = calculatorTriangleArea(height,base);
-    cout<<" the triangle area:"<<area<<endl;
-   
-    return 0;
+int main(){
+    double number1, number2, sum;
+    cout<<"enter the number1:"<<endl;
+    cin>>number1;
+    cout<<"enter the number2:"<<endl;
+    cin>>number2;
+    sum = sumTwoNumbers(number1,number2);
+    cout<<" the result: "<<sum<<endl;
+    return 0; 
 }
 
 
-//Write a function that takes the base and height of a triangle and return its area.
+//Create a function that takes two numbers as arguments and returns their sum.
+//Write a function that takes an integer minutes and converts it to seconds.
+//Create a function that takes the age in years and returns the age in days.
